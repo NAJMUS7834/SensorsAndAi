faff0f124e8fab6181f24b5650195e48f3a204b0
Najmus_Seemab <najmus.seemab@gmail.com>
2018-04-28 20:11:37 +0530

    Update SensorVisual.java

--- app/src/main/java/com/example/najmus/sensorsai/SensorVisual.java
+++ app/src/main/java/com/example/najmus/sensorsai/SensorVisual.java
@@ -1,4 +1,7 @@
 package com.example.najmus.sensorsai;
+/*====================================
+    Author : NAJMUS SEEMAB
+======================================*/
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
